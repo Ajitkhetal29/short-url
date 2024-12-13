4b2ea2cda797bb90fd6fd6b5de54dd37abb7f9cb
Ajitkhetal29 <ajitkhetalofficial@gmail.com>
2024-12-13 17:55:56 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const shortid = require("shortid");
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +10,9 @@ const URL = require("./models/url");
 
 const PORT = 8001;
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("mongodb connected")
 );
@@ -17,5 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/url", urlRouter);
+app.get("/test", async(req, res) => {
+  const allURLS = await URL.find({}) 
+  return res.render("home", {
+    urls : allURLS
+  })
+})
 
 app.listen(PORT, () => console.log(`server started on PORT : ${PORT}`));
